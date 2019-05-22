c916ae7bbbddf32da5f1b6aaae1963d98bcc44a5
unknown <love@megroup.in>
2019-05-23 03:31:24 +0530

    modified variable mutation

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* Variable  and datatypes */
+/*
 var FirstName = 'Love';
 console.log(FirstName);
 
@@ -12,3 +14,16 @@ console.log(job);
 
 job ='teacher';
 console.log(job);
+
+/* variable mutation  and type coercion */
+
+var FirstName ='Love';
+var age ='25';
+
+console.log(FirstName + ' ' + age );
+
+var job, ismarried;
+job = 'teacher';
+ismarried = false;
+
+console.log(FirstName +'is a ' + age +  'year old' +job + ' .Is he married?' + ismarried);
