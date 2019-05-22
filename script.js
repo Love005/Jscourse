8c55be904450ad20863f32af9337eeb8a3d992f4
unknown <love@megroup.in>
2019-05-22 14:52:22 +0530

    added var

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
- console.log('Hello World!!!');
+var FirstName = 'Love';
+console.log(FirstName);
+
+var lastName = 'Fist';
+var age = '25';
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job ='teacher';
+console.log(job);
